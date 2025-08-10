d9c775105053b4fca7e745fc796fbcf80380b7cf
stussy-cloud <stussy1406@gmail.com>
2025-08-10 22:07:29 +0900

    Update entities.js

--- entities.js
+++ entities.js
@@ -1,29 +1,26 @@
-const POP = PERF.low ? {res:6, npcs:10, animals:4} : {res:12, npcs:28, animals:12};
-let entities = [];
+// 軽量な群衆（ワールド座標）
+const POP = PERF.low ? {res:6,npcs:12} : {res:12,npcs:36};
+let dots=[];
 
-function initEntities() {
-  entities = [];
-  for (let i = 0; i < POP.npcs; i++) {
-    entities.push({
-      x: rand(0, W),
-      y: rand(0, H),
-      color: choice(['#ff595e', '#8ac926', '#1982c4']),
-    });
+function initEntities(){
+  dots=[];
+  for(let i=0;i<POP.npcs;i++){
+    dots.push({ x:rand(200,CONFIG.world.w-200), y:rand(200,CONFIG.world.h-200),
+      vx:rand(-0.6,0.6), vy:rand(-0.6,0.6), c:pick(['#ff7aa2','#ffd166','#7bb6ff','#7bd389']) });
   }
 }
 
-function updateEntities() {
-  for (let e of entities) {
-    e.x += rand(-1, 1);
-    e.y += rand(-1, 1);
+function updateEntities(dt){
+  for(const d of dots){
+    d.x+=d.vx; d.y+=d.vy;
+    if(d.x<100||d.x>CONFIG.world.w-100) d.vx*=-1;
+    if(d.y<100||d.y>CONFIG.world.h-100) d.vy*=-1;
   }
 }
 
-function drawEntities() {
-  for (let e of entities) {
-    ctx.fillStyle = e.color;
-    ctx.beginPath();
-    ctx.arc(e.x, e.y, 5, 0, Math.PI * 2);
-    ctx.fill();
+function drawEntities(){
+  for(const d of dots){
+    ctx.fillStyle=d.c;
+    ctx.beginPath(); ctx.arc(d.x,d.y,6,0,Math.PI*2); ctx.fill();
   }
 }
